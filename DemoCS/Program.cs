f5a16a3af79aa558cc3d3c15be6c0bfccefc0b05
tungnt2910 <tungnt2910@gmail.com>
2023-04-01 21:16:22 +0700

    Add project files.

---
+++ DemoCS/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DemoCS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
